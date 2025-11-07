5754ceae18cebbc27b06921253ddcb11e8bb3481
Dev Kew <koncasnet@gmail.com>
2025-11-08 06:20:35 +0700

    🧹 Clear cache and re-track all files

--- backend/CafeBookingAPI/Models/Table.cs
+++
@@ -1,9 +0,0 @@
-namespace CafeBookingAPI.Models
-{
-    public class Table
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = "";
-        public bool IsAvailable { get; set; } = true;
-    }
-}
