9558e3c1356084b7ecae4ce1839fcf7e4b92e217
Dev Kew <koncasnet@gmail.com>
2025-11-08 06:12:56 +0700

    🧹 Clear cache and re-track all files

--- frontend/cafe-booking-frontend/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { BookingComponent } from './pages/booking/booking.component';
-
-export const routes: Routes = [
-  { path: '', redirectTo: 'booking', pathMatch: 'full' },
-  { path: 'booking', component: BookingComponent },
-];
--- frontend/cafe-booking-frontend/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'https://pure-spirit-production.up.railway.app/api'
-};
